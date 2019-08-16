9033347f823ee5191ee57f736d3f24fe59075844
Hector Flores <Hector_Flores@eogresources.com>
2019-08-15 18:37:59 -0700

    Javascript -> two-sum.js

---
+++ easy/two-sum.js
@@ -0,0 +1,15 @@
+var twoSum = function(nums, target) {
+  const map = new Map()
+  for (let i = 0; i < nums.length; i++) {
+    let complement = target - nums[i]
+    if (map.has(complement)) {
+      return [map.get(complement), i]
+    }
+    map.set(nums[i], i)
+  }
+}
+
+const nums = [2, 7, 11, 15]
+const target = 9
+
+console.log(twoSum(nums, target))
